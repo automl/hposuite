b1a4fe5d728346ccd01d85298b79cf9768281e17
Sohambasu07 <soham.basu07@gmail.com>
2024-11-03 01:35:55 +0100

    Fixed and tested Pymoo SingleObjective Problems

--- hposuite/benchmarks/pymoo.py
+++ hposuite/benchmarks/pymoo.py
@@ -9,7 +9,6 @@ from typing import TYPE_CHECKING, Any
 
 import numpy as np
 from ConfigSpace import ConfigurationSpace, Float
-
 from hpoglue.benchmark import BenchmarkDescription, SurrogateBenchmark
 from hpoglue.env import Env
 from hpoglue.measure import Measure
@@ -17,31 +16,31 @@ from hpoglue.result import Result
 
 if TYPE_CHECKING:
     import pymoo
-
     from hpoglue.query import Query
 
 def get_pymoo_space(
-    pymoo_prob: pymoo.core.problem.Problem,
-    seed: int
+    pymoo_prob: pymoo.core.problem.Problem
 ) -> ConfigurationSpace:
     """Get ConfigSpace from pymoo problem."""
     n_var = pymoo_prob.n_var
     xl, xu = pymoo_prob.xl, pymoo_prob.xu
     hps = [Float(name=f"x{i}", bounds=[xl[i], xu[i]]) for i in range(n_var)]
-    configspace = ConfigurationSpace(seed=seed)
-    configspace.add_hyperparameters(hps)
-    return configspace
+    space = ConfigurationSpace()
+    space.add(hps)
+    return space
 
 
 def _pymoo_query_function(
     query: Query,
     benchmark: pymoo.core.problem.Problem,
 ) -> Result:
-    assert isinstance(query.fidelity, None)
+    assert query.fidelity is None
     config_vals = np.array(list(query.config.values.values()))
+    values = benchmark.evaluate(config_vals)[0]    #TODO: Handle this for multiobjective
+    values = {"value": values}
     return Result(
         query=query,
-        value=benchmark.evaluate(config_vals), # Convert to dict
+        values=values,
         fidelity=None,
     )
 
@@ -54,23 +53,17 @@ def _get_pymoo_problems(
 )-> SurrogateBenchmark:
 
     import pymoo
+    import pymoo.problems
 
     bench = pymoo.problems.get_problem(function_name, **kwargs)
     query_function = partial(_pymoo_query_function, benchmark=bench)
     return SurrogateBenchmark(
         desc=description,
         benchmark=bench,
-        config_space=partial(get_pymoo_space, pymoo_prob=bench),
+        config_space=get_pymoo_space(bench),
         query=query_function,
     )
 
-_pymoo_global_opt = [
-    "go-ackley01",
-    "go-ackley02",
-    "go-ackley03",
-    "go-branin01",
-    "go-branin02",
-]
 
 _pymoo_so = [
     "ackley",
@@ -87,10 +80,10 @@ _pymoo_so = [
 def pymoo_problems() -> Iterator[BenchmarkDescription]:
     env = Env(
         name="py310-pymoo-0.6.1.3",
-        requirements=["pymoo==0.6.1.3"],
+        requirements=("pymoo==0.6.1.3"),
         post_install=None
     )
-    for prob_name in _pymoo_global_opt + _pymoo_so:
+    for prob_name in _pymoo_so:
         yield BenchmarkDescription(
             name=f"pymoo-{prob_name}",
             env=env,

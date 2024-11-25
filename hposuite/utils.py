e956a8a40bb219b223d67d338ffd8aed8e74e551
Sohambasu07 <soham.basu07@gmail.com>
2024-11-26 00:02:46 +0100

    Minor changes in hposuite.utils

--- hposuite/utils.py
+++ hposuite/utils.py
@@ -3,24 +3,20 @@ from __future__ import annotations
 from typing import Any
 
 from hpoglue import Problem
-from hpoglue.budgets import CostBudget, TrialBudget
 
 
 class GlueWrapperFunctions:
     """A collection of wrapper functions around certain hpoglue methods."""
 
     @staticmethod
-    def problem_to_dict(problem: Problem) -> dict[str, Any]:
-        """Convert a Problem instance to a dictionary."""
-        return problem.to_dict()
-
-    @staticmethod
     def problem_from_dict(data: dict[str, Any]) -> Problem:
         """Convert a dictionary to a Problem instance."""
         from hposuite.benchmarks import BENCHMARKS
+        from hposuite.optimizers import OPTIMIZERS
 
         return Problem.from_dict(
             data=data,
-            benchmarks_dict=BENCHMARKS
+            benchmarks_dict=BENCHMARKS,
+            optimizers_dict=OPTIMIZERS,
         )
 

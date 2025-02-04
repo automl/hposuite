67530ea8e8139731a8585626f570ca7ddcf538bb
Sohambasu07 <soham.basu07@gmail.com>
2025-02-04 02:15:46 +0100

    Added rs with priors

--- examples/lib/optimizers/__init__.py
+++ examples/lib/optimizers/__init__.py
@@ -5,13 +5,14 @@ from typing import TYPE_CHECKING
 from lib.optimizers.ex_blackbox_opt import Ex_Blackbox_Opt
 from lib.optimizers.ex_mo_opt import Ex_MO_Opt
 from lib.optimizers.ex_multifidelity_opt import Ex_Multifidelity_Opt
-from lib.optimizers.random_search import RandomSearch
+from lib.optimizers.random_search import RandomSearch, RandomSearchWithPriors
 
 if TYPE_CHECKING:
     from hpoglue import Optimizer
 
 OPTIMIZERS: dict[str, type[Optimizer]] = {
     RandomSearch.name: RandomSearch,
+    RandomSearchWithPriors.name: RandomSearchWithPriors,
     Ex_MO_Opt.name: Ex_MO_Opt,
     Ex_Multifidelity_Opt.name: Ex_Multifidelity_Opt,
     Ex_Blackbox_Opt.name: Ex_Blackbox_Opt,

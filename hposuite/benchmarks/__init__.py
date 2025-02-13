a220fd935cd625e44f7240c688987784287335ec
Sohambasu07 <soham.basu07@gmail.com>
2025-02-13 17:04:50 +0100

    Added bbob noiseless single objective problems

--- hposuite/benchmarks/__init__.py
+++ hposuite/benchmarks/__init__.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+from hposuite.benchmarks.bbob import bbob_benchmarks
 from hposuite.benchmarks.mfp_bench import mfpbench_benchmarks
 from hposuite.benchmarks.pymoo import pymoo_benchmarks
 from hposuite.benchmarks.synthetic import ACKLEY_BENCH, BRANIN_BENCH
@@ -12,21 +13,25 @@ if TYPE_CHECKING:
 BENCHMARKS: dict[str, BenchmarkDescription] = {}
 MF_BENCHMARKS: dict[str, BenchmarkDescription] = {}
 NON_MF_BENCHMARKS: dict[str, BenchmarkDescription] = {}
+
+BENCHMARKS[ACKLEY_BENCH.desc.name] = ACKLEY_BENCH
+BENCHMARKS[BRANIN_BENCH.desc.name] = BRANIN_BENCH
+
 for desc in mfpbench_benchmarks():
     BENCHMARKS[desc.name] = desc
     if desc.fidelities is not None:
         MF_BENCHMARKS[desc.name] = desc
     else:
         NON_MF_BENCHMARKS[desc.name] = desc
+
 for desc in pymoo_benchmarks():
     BENCHMARKS[desc.name] = desc
-    if desc.fidelities is not None:
-        MF_BENCHMARKS[desc.name] = desc
-    else:
-        NON_MF_BENCHMARKS[desc.name] = desc
+    NON_MF_BENCHMARKS[desc.name] = desc
 
-BENCHMARKS[ACKLEY_BENCH.desc.name] = ACKLEY_BENCH
-BENCHMARKS[BRANIN_BENCH.desc.name] = BRANIN_BENCH
+for desc in bbob_benchmarks():
+    BENCHMARKS[desc.name] = desc
+    NON_MF_BENCHMARKS[desc.name] = desc
+
 
 __all__ = [
     "BENCHMARKS",

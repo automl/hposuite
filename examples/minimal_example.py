e5f963a4af562a2e62ca956f56b7fca0487bc0aa
Sohambasu07 <soham.basu07@gmail.com>
2025-03-17 21:17:26 +0100

    Updated minimal example

--- examples/minimal_example.py
+++ examples/minimal_example.py
@@ -3,12 +3,12 @@ from hposuite.study import create_study
 if __name__ == "__main__":
     study = create_study(
         name="minimal_example",
-        output_dir="example-outputs",
-    )
-    study.optimize(
+        output_dir="./examples/example-outputs",
         optimizers=("DEHB", {"eta": 3}),
         benchmarks="mfh3_good",
         seeds=1,
         budget=50,
+    )
+    study.optimize(
         overwrite=True,
     )
